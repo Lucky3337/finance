d9a544d71dc9983724fc3856bb1a8c2649f82eca
Vlad Bobrov <bobrov.v1ad@yandex.ru>
2019-04-04 01:47:15 +0300

    fixed access )

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     # path('accounts/login', auth_views.LoginView.as_view(), name='login'),
     # path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
 
+    # todo need to fix the bug with reset password by email
     # Password Reset Url's
     path('accounts/password_reset/', auth_views.PasswordResetView.as_view(
         success_url=reverse_lazy('password_reset_done')
--- finance/settings.py
+++ finance/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,6 +133,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.mail.ru'
-EMAIL_HOST_USER = 'ekstrosens123@mail.ru'
-EMAIL_HOST_PASSWORD = '22332200bel'
+EMAIL_HOST_USER = email['user']
+EMAIL_HOST_PASSWORD = email['password']
 EMAIL_PORT = 587
